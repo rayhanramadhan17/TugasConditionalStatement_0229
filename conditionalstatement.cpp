c4d217cfd1b7b6611fa7097347d1f3c6469b753b
rayhanramadhan17 <rayhanramadhan179072@gmail.com>
2026-03-09 20:28:03 +0800

    prosedurmemasukkandata

--- conditionalstatement.cpp
+++ conditionalstatement.cpp
@@ -1,2 +1,8 @@
 #include <iostream>
 using namespace std;
+// Prosedur untuk memasukkan data
+void inputData(float &berat, float &tinggi) {
+    cout << "Masukkan berat badan (kg): ";
+    cin >> berat;
+    cout << "Masukkan tinggi badan (meter): ";
+    cin >> tinggi;
